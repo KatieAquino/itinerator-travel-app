d78ac2c316ac4fefce5d52f193d882bd34c43652
Katie Aquino <katiemaryfrances.aquino@gmail.com>
2021-03-11 01:08:01 +0000

    Adds carousel to homepage

--- static/js/app.jsx
+++ static/js/app.jsx
@@ -14,25 +14,31 @@ const Homepage = (props) => {
   );
 }
 
-const NavBar = (props) => {
+const CarouselHome = (props) =>{
+  var Carousel = ReactBootstrap.Carousel
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <div className="nav-search">
-      <div className="form-group mb-2">
-        <form action="/api/destination/search" className="form-inline">
-          <div className="form-group mx-sm-3 mb-2">
-            <input 
-                  type="search"
-                  className="form-control"
-                  placeholder="Where are you heading?" 
-                  name="search_input"  />
-            <button type="button" className="btn btn-primary"> Go </button>
-          </div>
-        </form>
-      </div>
-    </div>
-    </div>
+    <Carousel>
+      <Carousel.Item interval={1000}>
+        <img  className="d-block w-100"
+              src={"static/images/shibuya-crossing.JPG"}
+              alt="Shibuya crossing in Tokyo, Japan during the rain." />
+      </Carousel.Item>
+      <Carousel.Item interval={1000}>
+        <img  className="d-block w-100"
+              src="static/images/Schladming-Austria.JPG"
+              alt="A view of the Dachstein mountain range from Schladming, Austria" />
+      </Carousel.Item>
+      <Carousel.Item interval={1000}>
+        <img  className="d-block w-100"
+              src="static/images/twelve-apostles.JPG"
+              alt="The Twelve Apostles in the Pacific Ocean near Victoria, Australia." />
+      </Carousel.Item>
+      <Carousel.Item interval={1000}>
+        <img  className="d-block w-100"
+              src="static/images/grand-central-terminal.JPG"
+              alt="A clock in Grand Central Terminal in New York City." />
+      </Carousel.Item>
+    </Carousel>
   );
 }
 
@@ -67,12 +73,10 @@ const TravelApp = (props) => {
   return (
     <Router>
       <div>
-        <nav>
-            <NavBar />
-        </nav>
         <Switch>
           <Route exact path="/">
             <Homepage />
+            <CarouselHome />
             <Login />
             <NewUser />
           </Route>
