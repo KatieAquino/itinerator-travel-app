7be78b61281cc0ad81a6da686e85fff3d5390a03
Katie Aquino <katiemaryfrances.aquino@gmail.com>
2021-03-14 01:38:29 +0000

    Adjusts sizing for carousel images and background of captions

--- static/js/app.jsx
+++ static/js/app.jsx
@@ -20,23 +20,40 @@ const CarouselHome = (props) =>{
     <Carousel>
       <Carousel.Item interval={2000}>
         <img  className="d-block w-100"
-              src={"static/images/shibuya-crossing.JPG"}
+              src="static/images/shibuya-crossing-1.JPG"
               alt="Shibuya crossing in Tokyo, Japan during the rain." />
+        <Carousel.Caption>
+          <h3>Boldy Travel</h3>
+          <UserAccount />
+        </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item interval={2000}>
         <img  className="d-block w-100"
               src="static/images/Schladming-Austria.JPG"
               alt="A view of the Dachstein mountain range from Schladming, Austria" />
+        <Carousel.Caption>
+          <h3>Boldy Travel</h3>
+          <UserAccount />
+        </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item interval={2000}>
         <img  className="d-block w-100"
-              src="static/images/twelve-apostles.JPG"
+              src="static/images/twelve-apostles.png"
               alt="The Twelve Apostles in the Pacific Ocean near Victoria, Australia." />
+      <Carousel.Caption>
+          <h3>Boldy Travel</h3>
+          <UserAccount />
+        </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item interval={2000}>
         <img  className="d-block w-100"
-              src="static/images/grand-central-terminal.JPG"
+              src="static/images/grand-central-terminal.png"
               alt="A clock in Grand Central Terminal in New York City." />
+      <Carousel.Caption>
+          <h3>Boldy Travel</h3>
+          <UserAccount />
+        </Carousel.Caption>
+      
       </Carousel.Item>
 
     </Carousel>
@@ -49,25 +66,36 @@ const Login = (props) => {
       Returning User
         <p>
           <Link to="/login-user">
-            <button type="button" className="btn btn-primary">Login</button>
+            <button type="button" 
+                    className="btn btn-primary">Login</button>
           </Link>
         </p>
     </section>
   );
 }
 
-const NewUser = (props) => {
+const UserAccount = (props) => {
   return (
     <section>
+      <div>
+      Returning User
+        <p>
+          <Link to="/login-user">
+            <button type="button" 
+                    className="btn btn-primary">Login</button>
+          </Link>
+        </p>
       New User
         <p>
           <Link to="/create-account">
             <button type="button" className="btn btn-primary">Create Account</button>
           </Link>
         </p>
+        </div>
     </section>
   );
 }
+
 
 const TravelApp = (props) => {
   
@@ -78,8 +106,8 @@ const TravelApp = (props) => {
           <Route exact path="/">
             <Homepage />
             <CarouselHome />
-            <Login />
-            <NewUser />
+            {/* <Login />
+            <NewUser /> */}
           </Route>
           <Route path="/login-user">
             <UserLogin />
